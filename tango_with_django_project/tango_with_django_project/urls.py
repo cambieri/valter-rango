3ff2f034239f67c04a27a2ef87903787b0c0547f
Valter Cambieri <valter@cambieri.it>
2014-06-02 03:14:19 +0200

    Templates, Static, Media

--- tango_with_django_project/tango_with_django_project/urls.py
+++ tango_with_django_project/tango_with_django_project/urls.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.conf.urls import patterns, include, url
 
 # Uncomment the next two lines to enable the admin:
@@ -5,13 +6,22 @@ from django.conf.urls import patterns, include, url
 # admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'tango_with_django_project.views.home', name='home'),
-    # url(r'^tango_with_django_project/', include('tango_with_django_project.foo.urls')),
+                       # Examples:
+                       # url(r'^$', 'tango_with_django_project.views.home', name='home'),
+                       # url(r'^tango_with_django_project/', include('tango_with_django_project.foo.urls')),
 
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+                       # Uncomment the admin/doc line below to enable admin documentation:
+                       # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
-    # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+                       # Uncomment the next line to enable the admin:
+                       # url(r'^admin/', include(admin.site.urls)),
+
+                       url(r'^rango/', include('rango.urls')),
 )
+
+if settings.DEBUG:
+	urlpatterns += patterns(
+		'django.views.static',
+		(r'media/(?P<path>.*)',
+		 'serve',
+		 {'document_root': settings.MEDIA_ROOT}), )
